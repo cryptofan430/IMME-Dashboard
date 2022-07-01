cd50a96c1c1cdc14537d6a12d1b994499918e11d
cryptoPanda430 <panda.kk1114@gmail.com>
2022-07-01 03:30:35 -0400

    Add wallet connect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect} from 'react';
+import {ethers} from 'ethers';
 
 function App() {
+  const [haveMetamask, setHaveMetamask] = useState(true);
+  const [accountAddress, setAccountAddress] = useState('');
+  const [accountBalance, setAccountBalance] = useState('');
+  const [isConnected, setIsConnected] = useState(false);
+
+  const { ethereum } = window;
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  useEffect(()=>{
+    const { ethereum } = window;
+    const checkMetamaskAvailability = async () => {
+      if (!ethereum) {
+        setHaveMetamask(false);
+      }
+      setHaveMetamask(true);
+    };
+    checkMetamaskAvailability();
+  },[])
+
+  
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) {
+        setHaveMetamask(false);
+      }
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      let balance = await provider.getBalance(accounts[0]);
+      let bal = ethers.utils.formatEther(balance);
+      setAccountAddress(accounts[0]);
+      setAccountBalance(bal);
+      setIsConnected(true);
+    } catch (error) {
+      setIsConnected(false);
+    }
+  };
   return (
     <div className="App">
-      
+      <header className="App-header">
+        {haveMetamask ? (
+          <div className="App-header">
+            {isConnected ? (
+              <div className="card">
+                <div className="card-row">
+                  <h3>Wallet Address:</h3>
+                  <p>
+                    {accountAddress.slice(0, 4)}...
+                    {accountAddress.slice(38, 42)}
+                  </p>
+                </div>
+                <div className="card-row">
+                  <h3>Wallet Balance:</h3>
+                  <p>{accountBalance}</p>
+                </div>
+              </div>
+            ) : (
+              'Not connected'
+            )}
+            {isConnected ? (
+              <p className="info">🎉 Connected Successfully</p>
+            ) : (
+              <button className="btn" onClick={connectWallet}>
+                Connect
+              </button>
+            )}
+          </div>
+        ) : (
+          <p>Please Install MataMask</p>
+        )}
+      </header>
     </div>
   );
 }
